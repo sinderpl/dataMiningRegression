12b9af12751999cdc1570c0e7f9a12a24f7ddb4d
Alan <sinderpl@gmail.com>
2020-11-24 18:03:07 +0000

    correlation

--- scripts/LinearRegression.R
+++ scripts/LinearRegression.R
@@ -1,6 +1,10 @@
-credit_data <- read.table("data/SouthGermanCredit.asc", header=TRUE) 
-pairs(credit_data)
-colnames(credit_data)
-data_model <- lm(kredit~.,data=credit_data)
+med_gpa <- read.csv("data/MedGPA.csv", strip.white=TRUE)
+
+# We drop the non numeric variables for pairs generation
+updated_med_gpa = subset(med_gpa, select = -c(X,Accept, Sex)) #Delete
+
+pairs(subset(med_gpa, select = -c(X,Accept, Sex)))
+colnames(updated_med_gpa)
+data_model <- lm(Acceptance~.,data=med_gpa)
 data_model$coefficients
 summary(data_model)
