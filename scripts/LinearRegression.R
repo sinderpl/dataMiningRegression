741f8255561457c338df0666d82d9ecb47764abe
Alan <sinderpl@gmail.com>
2020-11-24 18:04:53 +0000

    updated column names for correlation

--- scripts/LinearRegression.R
+++ scripts/LinearRegression.R
@@ -1,9 +1,10 @@
 med_gpa <- read.csv("data/MedGPA.csv", strip.white=TRUE)
-
+readable_column_names <- c("Acceptance", "Bio, Chem, Physics, Maths", "GPA", "Verbal \n Reasoning",
+                          "Physical \n Science", "Writing \n sample", "Bio\n Science",  "MCAT", "Applications\n Made")
 # We drop the non numeric variables for pairs generation
 updated_med_gpa = subset(med_gpa, select = -c(X,Accept, Sex)) #Delete
 
-pairs(subset(med_gpa, select = -c(X,Accept, Sex)))
+pairs(subset(med_gpa, select = -c(X,Accept, Sex)), labels =readable_column_names)
 colnames(updated_med_gpa)
 data_model <- lm(Acceptance~.,data=med_gpa)
 data_model$coefficients
