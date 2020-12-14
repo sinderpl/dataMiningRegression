2b7823a87210014a73907ac25bf8b79add94b319
Alan <sinderpl@gmail.com>
2020-12-14 20:45:08 +0000

    split into test and train

--- endTermAssesment/decision_trees.R
+++ endTermAssesment/decision_trees.R
@@ -5,3 +5,14 @@ library(Metrics)
 
 
 customer_data <- read.csv("data/telco_churn.csv", strip.white=TRUE)
+
+#Check summaries and idenitify rows with null values
+customer_data[!complete.cases(customer_data),]
+customer_data <- customer_data[complete.cases(customer_data),]
+
+#Data summary and exploration
+summary(customer_data)
+
+#Split into test and training data
+customer_data_train <- customer_data [1:7000,]
+customer_data_test <- customer_data[7000:7032,]
